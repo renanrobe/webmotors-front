dcf05517b85a46ffd4a0a581d5c02f2934c8c7f7
Renan Roberto <renanrobe@gmail.com>
2020-07-28 14:29:21 -0300

    Updates

---
+++ precache-manifest.92c48b6f3c3aaf33cbed7ffa118480a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6bd40c426e158bacd5e4ac0a2b6e6e",
+    "url": "/webmotors-front/index.html"
+  },
+  {
+    "revision": "5b05f89319b9c6d9b1db",
+    "url": "/webmotors-front/static/css/main.ed0933c9.chunk.css"
+  },
+  {
+    "revision": "05bc8ab03fc67e79ee9b",
+    "url": "/webmotors-front/static/js/2.6d2a7c01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webmotors-front/static/js/2.6d2a7c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b05f89319b9c6d9b1db",
+    "url": "/webmotors-front/static/js/main.56f3a1bf.chunk.js"
+  },
+  {
+    "revision": "194aeb6dd9fe0629bb8f",
+    "url": "/webmotors-front/static/js/runtime-main.4be34b6a.js"
+  },
+  {
+    "revision": "ef770bc7dcbd611d1eaf7be5bcdb7863",
+    "url": "/webmotors-front/static/media/logo.ef770bc7.svg"
+  }
+]);
